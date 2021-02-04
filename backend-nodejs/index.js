2996ccccdbb01851a14bd0d8c08b43a63c41cf2e
popehiflo <poolpetterhijuel@hotmail.com>
2021-02-04 11:45:00 -0500

    Se implementa ruta raiz, request GET

--- backend-nodejs/index.js
+++ backend-nodejs/index.js
@@ -3,6 +3,14 @@ const express = require('express');
 // Crear el servidor de express
 const app = express();
 
+// Rutas
+app.get('/', (req, res) => {
+    res.json({
+        ok: true,
+        msg: 'Hola Mundo!'
+    })
+});
+
 app.listen(3000, () => {
     console.log('Serve node iniciado en puerto ' + 3000);
 })
