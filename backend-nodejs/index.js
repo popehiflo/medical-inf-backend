e8bb8dcc8df0c859251bb139159fd59af4903141
popehiflo <poolpetterhijuel@hotmail.com>
2021-02-05 01:27:31 -0500

    Se instala dependencia 'cors', que configura cors(use - middleware)

--- backend-nodejs/index.js
+++ backend-nodejs/index.js
@@ -1,10 +1,15 @@
-const express = require('express');
 require('dotenv').config();
+
+const express = require('express');
+const cors = require('cors');
 
 const { dbConnection } = require('./database/config');
 
 // Crear el servidor de express
 const app = express();
+
+// Configurar CORS
+app.use(cors());
 
 // Base de Datos
 dbConnection();
