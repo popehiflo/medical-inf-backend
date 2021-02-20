0646583bf9dea2763381e2988ff196f198fe6d09
popehiflo <poolpetterhijuel@hotmail.com>
2021-02-20 13:28:47 -0500

    Valida campos antes de insertar a db

--- backend/api-nodejs/gestionclinica-nodejs/routes/usuarios.js
+++ backend/api-nodejs/gestionclinica-nodejs/routes/usuarios.js
@@ -2,6 +2,9 @@
     Ruta: /api/usuarios
 */
 const { Router } = require('express');
+const { check } = require('express-validator');
+const { validarCampos } = require('../middlewares/validar-campos');
+
 const { getUsuarios, crearUsuario } = require('../controllers/usuario');
 
 const router = Router();
@@ -11,7 +14,13 @@ const router = Router();
 router.get( '/' , getUsuarios );
 
 // Crear Usuario
-router.post( '/', 
+router.post( '/',
+    [
+        check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+        check('password', 'El password es obligatorio').not().isEmpty(),
+        check('email', 'El email es obligatorio').isEmail(),
+        validarCampos,
+    ], 
     crearUsuario 
 );
 
