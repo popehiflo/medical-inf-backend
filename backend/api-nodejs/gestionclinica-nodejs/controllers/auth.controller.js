940e6fffe852fdd1a9eb789ae0b0484caa1819c4
popehiflo <poolpetterhijuel@hotmail.com>
2021-03-06 11:22:12 -0500

    Genera token para usuario al hacer login

--- backend/api-nodejs/gestionclinica-nodejs/controllers/auth.controller.js
+++ backend/api-nodejs/gestionclinica-nodejs/controllers/auth.controller.js
@@ -2,6 +2,7 @@ const { response } = require('express');
 const bcrypt = require('bcryptjs');
 
 const Usuario = require('../models/usuario.model');
+const { generarJWT } = require('../helpers/jwt');
 
 
 const login = async( req, res = response ) => {
@@ -28,7 +29,7 @@ const login = async( req, res = response ) => {
         }
 
         // Generar el TOKEN - JWT
-        
+        const token = await generarJWT( usuarioDB.id );
 
         res.json({
             ok: true,
