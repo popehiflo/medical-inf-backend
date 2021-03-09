917a957cc7ab99f4a65c304fcf6d0e533fccffec
popehiflo <poolpetterhijuel@hotmail.com>
2021-03-08 23:02:52 -0500

    Controlador para Busqueda global

---
+++ backend/api-nodejs/gestionclinica-nodejs/controllers/busqueda.controller.js
@@ -0,0 +1,18 @@
+const { response } = require("express");
+
+
+const getBusquedas = async (req, res = response) => {
+
+    // Listar Documentos encontrados
+    const buscar = req.params.buscar;
+
+    res.status(200).json({
+        ok: true,
+        msg: 'getBusquedas',
+        buscar
+    })
+}
+
+module.exports = {
+    getBusquedas
+}
